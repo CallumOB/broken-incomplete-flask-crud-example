e77aa42759f8eb9ba2196c067f3c56796cb41377
CallumOB <callumobrien2004@gmail.com>
2023-11-27 12:28:07 +0000

    lab5 done and code fixes

--- app.py
+++ app.py
@@ -8,22 +8,27 @@ app = Flask(__name__)
 CORS(app)
 # My SQL Instance configurations
 # Change these details to match your instance configurations
-app.config['MYSQL_USER'] = 'A'
-app.config['MYSQL_PASSWORD'] = 'B'
+app.config['MYSQL_USER'] = 'user'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'db.mydomain.ie'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
 def add():
   name = request.args.get('name')
   email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
+  if name == None or email == None: # added a check to be sure no useless data is added to the db
+    return 'Please specify a name and email ...'
+  
+  else:
+    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
+    cur.execute(s)
+    mysql.connection.commit()
+
+    return '{"Result":"Success"}' # Really? maybe we should check!
   
 @app.route("/") #Default - Show Data
 def read(): # Name of the method
@@ -39,7 +44,7 @@ def read(): # Name of the method
     Results.append(Result)
   response={'Results':Results, 'count':len(Results)}
   ret=app.response_class(
-    response=json.dumps(response),
+    response=json.dumps(response, indent=2), # added 'indent=2' for nice formatting
     status=200,
     mimetype='application/json'
   )
